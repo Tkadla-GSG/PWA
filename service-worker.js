f9e3e0c61a324190172cb5e2fe7966c37cf5926c
Jakub Tkadlecek <jakub@mewssystems.com>
2017-11-07 00:08:04 +0100

    fetch caching

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,19 @@
-var cacheName = 'v1';
-var filesToCache = ['./', './index.html', './image/img.jpg', './manifest.json', './service-worker.js'];
+const cacheName = 'v1';
+const filesToCache = ['./', './index.html', './image/img.jpg', './manifest.json', './service-worker.js'];
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(cacheName).then(cache => {
-        return cache.addAll(filesToCache);
-    }).then(() => {
-      return self.skipWaiting();
-    })
-  );
+    event.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll(filesToCache);
+        }).then(() => {
+            return self.skipWaiting();
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+        })
+    );
 });
